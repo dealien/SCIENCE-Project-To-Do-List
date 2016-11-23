92fec1119c4a145ac22512c71c478aa549984535
dealien <dragonofawsome@gmail.com>
2016-11-23 18:18:13 +0000

    Delete linkConversion.js

--- linkConversion.js
+++
@@ -1,6 +0,0 @@
-$(document).load(function() {
-    $('span.user').replaceWith(function() {
-        var user = $.trim($(this).text());
-        return '<a href="https://socialclub.rockstargames.com/member/' + user + '" target="_blank">' + user + '</a>';
-    });
-})
