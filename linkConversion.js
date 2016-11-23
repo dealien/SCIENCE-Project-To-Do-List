ef8120395fb6b8d1aa37a227979ece3a18cb00d9
dealien <dragonofawsome@gmail.com>
2016-11-23 02:25:54 +0000

    Update linkConversion.js

--- linkConversion.js
+++ linkConversion.js
@@ -1,4 +1,4 @@
-$(window).load(function() {
+$(document).load(function() {
     $('.sampleClass span').replaceWith(function() {
         var user = $.trim($(this).text());
         return '<a href="https://socialclub.rockstargames.com/member/' + user + '" target="_blank">' + user + '</a>';
