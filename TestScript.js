fdf95724706f86be1c59d7d83a68d38b77a3d1f5
dealien <dragonofawsome@gmail.com>
2016-11-25 15:18:26 +0000

    Update TestScript.js

--- TestScript.js
+++ TestScript.js
@@ -1,12 +1,12 @@
 document.body.onload = addElement;
 
 var nodeText = "This is the new text element.";
-console.log("nodeText = " + nodeTest)
+console.log("nodeText = " + nodeText)
 
 
 function addElement () { 
   var para = document.createElement("p");
-  var node = document.createTextNode("This is new.");
+  var node = document.createTextNode(nodeText);
   para.appendChild(node);
 
   var element = document.getElementById("div1");
