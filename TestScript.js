ed810a0a371612006472bcc70c126660241d8129
dealien <dragonofawsome@gmail.com>
2016-11-25 15:16:48 +0000

    Update TestScript.js

--- TestScript.js
+++ TestScript.js
@@ -1,5 +1,8 @@
 document.body.onload = addElement;
 
+var nodeText = "This is the new text element.";
+
+
 function addElement () { 
   var para = document.createElement("p");
   var node = document.createTextNode("This is new.");
