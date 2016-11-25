c35bac7ee945c56e7ab19773f0b67b9c7af0e80b
dealien <dragonofawsome@gmail.com>
2016-11-25 15:31:48 +0000

    Update TestScript.js

--- TestScript.js
+++ TestScript.js
@@ -16,9 +16,5 @@ function addElement() {
 function testjQuery() {
   if (!window.jQuery) {
     console.log("jQuery is not loaded");
-    var script = document.createElement('script');
-    script.type = "text/javascript";
-    script.src = "path/to/jQuery";
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
   } else {
     console.log("jQuery is loaded");
