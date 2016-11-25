64569a5e5d63e336fd0bfd9e6d13c13d29986945
dealien <dragonofawsome@gmail.com>
2016-11-25 15:31:32 +0000

    Update TestScript.js

--- TestScript.js
+++ TestScript.js
@@ -3,8 +3,7 @@ document.body.onload = addElement;
 var nodeText = "This is the new text element.";
 console.log("nodeText = " + nodeText);
 
-
-function addElement () { 
+function addElement() {
   console.log("Running function addElement()");
   var para = document.createElement("p");
   var node = document.createTextNode(nodeText);
@@ -13,3 +12,13 @@ function addElement () {
   var element = document.getElementById("div1");
   element.appendChild(para);
 }
+
+function testjQuery() {
+  if (!window.jQuery) {
+    console.log("jQuery is not loaded");
+    var script = document.createElement('script');
+    script.type = "text/javascript";
+    script.src = "path/to/jQuery";
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+  } else {
+    console.log("jQuery is loaded");
