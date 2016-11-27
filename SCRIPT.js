0d30671586211ec2ed097373df46a7c23f70a4ce
dealien <dragonofawsome@gmail.com>
2016-11-27 17:14:33 +0000

    Update SCRIPT.js

--- SCRIPT.js
+++ SCRIPT.js
@@ -1,6 +1,7 @@
 document.body.onload = addElement;
 document.body.onload = testjQuery;
 document.body.onload = linkNames;
+document.body.onload = crewTags;
 
 var nodeText = "This is the new text element.";
 console.log("nodeText = " + nodeText);
@@ -28,5 +29,30 @@ function linkNames() {
   $('span:not([class])').replaceWith(function() {
     var username = $.trim($(this).text());
     return '<a href="https://socialclub.rockstargames.com/member/' + username + '" target="_blank">' + username + '</a>';
+  });
+}
+
+function crewTags() {
+  console.log("Running function crewTags()");
+  $('span.crew').replaceWith(function() {
+    var val = $.trim($(this).text());
+    console.log("val = " + val);
+    var rank = (val.match(/\d+\.\d+|\d+\b|\d+(?=\w)/g) || []).map(function(v) {
+      return +v;
+    }).shift();
+    if (rank > 5) {
+      rank = 5
+      console.log("'rank' too large. Changed to '5'");
+    }
+    console.log("rank = " + rank);
+    var crew = val.substr(2);
+    var tag = '<div class="crewTag private"><span>' + crew + '</span><div class="hierarchy" title="">'
+    var i, j, ref;
+    for (i = j = 1, ref = rank; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
+      tag += '<span style="background:#bf11de;"></span>';
+    }
+    tag += '</span></div></div>'
+
+    return tag;
   });
 }
